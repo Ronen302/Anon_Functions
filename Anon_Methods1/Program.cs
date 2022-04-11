db04bd40a94fba0c9e656ed5298a9267a9617147
sapunov2020 <sapunov2020@tk34.mskobr.ru>
2022-04-11 10:33:19 +0300

    123

---
+++ Anon_Methods1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Anon_Methods1
+{
+    public delegate int AddDelegate(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddDelegate aD = new AddDelegate(delegate (int num1, int num2)
+            {
+                int res = num1 + num2;
+                return res;
+            });
+            int sum = aD(5, 4);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DelegatePredpTechnic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DelegatePredpTechnic
+{
+    public delegate int StrDelegate(string s);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StrDelegate sD = delegate (string str)
+            {
+                if (str == null)
+                    throw new Exception("Строка отсутствует");
+                else
+                    return str.Length;
+            };
+            Console.WriteLine(sD(""));
+            Console.ReadKey();
+        }
+    }
+}
